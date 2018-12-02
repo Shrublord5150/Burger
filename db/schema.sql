b97561e95924ec2d4614555c9508e8b93a9ded6f
Daniel Murphy <tugs.murphy@yahoo.com>
2018-12-01 22:46:23 -0500

    initial push, db created and working, added connections orm controller, need to add views

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(55),
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ('Jumpin Jalapeno');
+INSERT INTO burgers (burger_name) VALUES ('Plain Jane');
+INSERT INTO burgers (burger_name) VALUES ('Bubba Bacon Beast Destroyer of Worlds');
+INSERT INTO burgers (burger_name) VALUES ('Breakfast Burger of Champions');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Bills Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mightily and Meatless');
+INSERT INTO burgers (burger_name) VALUES ('Phat Friar');
+INSERT INTO burgers (burger_name) VALUES ('The Classic Classic');
